8a8540dc693e5664e39a788033da7ee66a0dcdcf
szeArtur <artur.szeczinowski@proton.me>
2025-10-10 18:09:48 +0200

    sticky player movement kind of working

--- scripts/agents/player.gd
+++ scripts/agents/player.gd
@@ -3,6 +3,8 @@ extends Agent
 
 
 @export var inventory: Inventory
+@export var wheel1: ShapeCast2D
+@export var wheel2: ShapeCast2D
 
 
 func _on_hitbox_entered(body: CollisionObject2D) -> void:
@@ -10,6 +12,22 @@ func _on_hitbox_entered(body: CollisionObject2D) -> void:
 		inventory.add_item(body.item, body.amount)
 
 
-func _physics_process(delta: float) -> void:
-	var input_direction := Input.get_axis("move_left", "move_right")
-	movement_controller.move_and_slide_toward(Vector2(input_direction,0), delta)
+func _physics_process(_delta: float) -> void:
+	velocity = global_transform.basis_xform(Vector2.RIGHT * Input.get_axis("move_left", "move_right")) * 60
+	move_and_slide()
+	
+	var new_slope_direction
+	if not wheel1.is_colliding():
+		rotation += PI/4
+	elif not wheel2.is_colliding():
+		rotation -= PI/4
+	else:
+		new_slope_direction = wheel1.get_collision_point(0) - wheel2.get_collision_point(0)
+		rotation = new_slope_direction.angle()
+	
+	#var wheel1_position := wheel1.get_collision_point(0) if wheel1.is_colliding() else (wheel1.global_position + wheel1.target_position)
+	#var wheel2_position := wheel2.get_collision_point(0) if wheel2.is_colliding() else (wheel2.global_position + wheel2.target_position) 
+	#var new_slope_direction := wheel1_position - wheel2_position
+	#rotation = new_slope_direction.angle()
+	
+	move_and_collide(global_transform.basis_xform(Vector2.DOWN))
