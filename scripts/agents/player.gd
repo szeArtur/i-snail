b6bc897c25a3dce4d08147e772dde0e1e81bfab3
szeArtur <artur.szeczinowski@proton.me>
2025-10-10 18:50:46 +0200

    player falls on overhang

--- scripts/agents/player.gd
+++ scripts/agents/player.gd
@@ -1,10 +1,13 @@
 class_name Player
 extends Agent
 
-
+@export var movement_speed := 60
 @export var inventory: Inventory
 @export var wheel1: ShapeCast2D
 @export var wheel2: ShapeCast2D
+
+var falling := true
+
 
 
 func _on_hitbox_entered(body: CollisionObject2D) -> void:
@@ -12,22 +15,29 @@ func _on_hitbox_entered(body: CollisionObject2D) -> void:
 		inventory.add_item(body.item, body.amount)
 
 
-func _physics_process(_delta: float) -> void:
-	velocity = global_transform.basis_xform(Vector2.RIGHT * Input.get_axis("move_left", "move_right")) * 60
-	move_and_slide()
+func _physics_process(delta: float) -> void:
+	var input_direction = Vector2.RIGHT * Input.get_axis("move_left", "move_right") * movement_speed
 	
-	var new_slope_direction
-	if not wheel1.is_colliding():
-		rotation += PI/4
-	elif not wheel2.is_colliding():
-		rotation -= PI/4
+	if falling:
+		velocity += get_gravity() * delta
+		if move_and_slide():
+			falling = false
+	
 	else:
-		new_slope_direction = wheel1.get_collision_point(0) - wheel2.get_collision_point(0)
-		rotation = new_slope_direction.angle()
+		velocity = global_transform.basis_xform(input_direction)
+		move_and_slide()
+			
+		if not wheel1.is_colliding():
+			rotation += PI/4
+		elif not wheel2.is_colliding():
+			rotation -= PI/4
+		elif wheel1.is_colliding() and wheel2.is_colliding():
+			var new_slope_direction = wheel1.get_collision_point(0) - wheel2.get_collision_point(0)
+			rotation = new_slope_direction.angle()
+			
+		move_and_collide(global_transform.basis_xform(Vector2.DOWN))
 	
-	#var wheel1_position := wheel1.get_collision_point(0) if wheel1.is_colliding() else (wheel1.global_position + wheel1.target_position)
-	#var wheel2_position := wheel2.get_collision_point(0) if wheel2.is_colliding() else (wheel2.global_position + wheel2.target_position) 
-	#var new_slope_direction := wheel1_position - wheel2_position
-	#rotation = new_slope_direction.angle()
+	if cos(rotation) < -0.2 :
+		rotation = 0
+		falling = true
 	
-	move_and_collide(global_transform.basis_xform(Vector2.DOWN))
