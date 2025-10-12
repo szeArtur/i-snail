a78228811d34174a6137da895d9888668fa3e362
schulschwatzer <schulschwatzer@gmail.com>
2025-10-13 00:12:23 +0200

    o

--- scripts/Text.gd
+++ scripts/Text.gd
@@ -2,6 +2,9 @@ extends Label
 @export var alles_text: Dialoque 
 @onready var v_box_container: VBoxContainer = $".."
 @export var time_betwen_cahr :=0.1
+@onready var left: Sprite2D = $"../../../Left"
+@onready var mid: Sprite2D = $"../../../Mid"
+@onready var right: Sprite2D = $"../../../Right"
 var time_already=0
 var text_number = 0
 var time_till_next_box =0
@@ -12,13 +15,23 @@ func _ready() -> void:
 	#set_clip_text(true)  # optional, falls du lieber abschneiden willst statt umbrechen
 	visible_characters=0
 	text= alles_text.messages[0].text
+	
 	match alles_text.messages[0].position:
 		0:
 			horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+			left.set_modulate(Color(1, 1, 1, 1.0))
+			mid.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
+			right.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
 		1:
 			horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+			left.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
+			mid.set_modulate(Color(1, 1, 1, 1.0))
+			right.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
 		2:
 			horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+			left.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
+			mid.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
+			right.set_modulate(Color(1, 1, 1, 1.0))
 	match alles_text.messages[0].position:
 		0:
 			size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
@@ -42,10 +55,19 @@ func _unhandled_input(_event: InputEvent) -> void:
 			match alles_text.messages[text_number].position:
 				0:
 					horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+					left.set_modulate(Color(1, 1, 1, 1.0))
+					mid.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
+					right.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
 				1:
 					horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+					left.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
+					mid.set_modulate(Color(1, 1, 1, 1.0))
+					right.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
 				2:
 					horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+					left.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
+					mid.set_modulate(Color(0.54, 0.54, 0.54, 1.0))
+					right.set_modulate(Color(1, 1, 1, 1.0))
 			match alles_text.messages[text_number].position:
 				0:
 					size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
