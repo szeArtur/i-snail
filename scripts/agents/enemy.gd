4beda6ffe7b9c609a52892f08a36335cd90b9e9f
schulschwatzer <schulschwatzer@gmail.com>
2025-10-12 15:50:39 +0200

    gegner vision und sprite

--- scripts/agents/enemy.gd
+++ scripts/agents/enemy.gd
@@ -1,9 +1,11 @@
 class_name Enemy
 extends Agent
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_cast_2d: RayCast2D = $AnimatedSprite2D/RayCast2D
 
 @export var jumpforce := 10.0
 @export var start_links:= true
-
+var player_senn_prog =0
 var movement_direction
 
 
@@ -15,8 +17,20 @@ func _ready() -> void:
 		movement_direction = 1
 
 func _physics_process(delta: float) -> void:
-	movement_controller.move(delta, movement_direction, true)
-
+	if ray_cast_2d.is_colliding():
+		if ray_cast_2d.get_collider() is Player:
+			player_senn_prog=1
+	if player_senn_prog != 1:
+		movement_controller.move(delta, movement_direction, true)
+	else: 
+		animated_sprite_2d.play("button")
+	
+		
 func _on_hitbox_entered(body: CollisionObject2D) -> void:
 	if body is TurnaroundEnemy:
 		movement_direction *= -1
+
+
+func _on_visionbox_body_entered(_body: Node2D) -> void:
+	if player_senn_prog == 0:
+		player_senn_prog=1
