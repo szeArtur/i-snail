30d3fc8b20b26ce1b2337ddfca0232ac99785296
szeArtur <artur.szeczinowski@proton.me>
2025-10-13 22:29:16 +0200

    redid intro with animation player

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,16 +1,4 @@
 extends VideoStreamPlayer
 
-var dissolve := false
-
-
-func _process(delta: float) -> void:
-	if dissolve:
-		modulate = lerp(modulate, Color.TRANSPARENT, delta * 2)
-
-func _on_finished() -> void:
-	queue_free()
-
-
-func _on_timer_timeout() -> void:
+func switch_to_menu() -> void:
 	GameManager.change_state(GameManager.GameState.MAIN_MENU)
-	dissolve = true
--- scripts/timer01.gd
+++
@@ -1,8 +0,0 @@
-extends AudioStreamPlayer
-
-func _on_timer_timeout() -> void:
-	play()
-
-
-func _on_finished() -> void:
-	$AudioStreamPlayer2D.playing = true
