34dfb4ebcca6121c31b05cdfbc698a3d50c3610b
szeArtur <artur.szeczinowski@proton.me>
2025-10-15 18:20:13 +0200

    refactored and fixed jumppad script

--- scripts/jump_pad.gd
+++ scripts/jump_pad.gd
@@ -1,13 +1,18 @@
 class_name JumpPad
 extends Area2D
+
+
 @onready var anime_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var force:= 5000.0
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-func activate():
-	anime_sprite.play()
-	audio_stream_player_2d.play()
+@export var force := 800.0
+
 
 func _on_body_entered(body: Node2D) -> void:
-	body.jumppad(force)
-	activate()
+	if body is Agent:
+		body.velocity.y = -force
+	if body is RigidBody2D:
+		body.set_axis_velocity(Vector2.UP * force)
+	
+	anime_sprite.play()
+	audio_stream_player_2d.play()
