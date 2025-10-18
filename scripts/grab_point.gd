e1240e1cbb64c27b4c161842b589d6c5ef073f62
szeArtur <artur.szeczinowski@proton.me>
2025-10-18 10:24:03 +0200

    small changes to grab point logic

--- scripts/grab_point.gd
+++ scripts/grab_point.gd
@@ -1,10 +1,13 @@
 class_name GrabPoint
 extends Marker2D
 
+
 @export var label: Label
 
-func _init() -> void:
-	add_to_group("GrabPoints")
+
+func _ready() -> void:
+	update_label()
+
 
 func update_label() -> void:
 	label.text = "grab (" + InputMap.get_action_description("interact") + ")"
