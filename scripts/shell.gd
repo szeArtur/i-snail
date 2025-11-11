a5161e8feeda73a980c8349093679519a70670f3
szeArtur <artur.szeczinowski@proton.me>
2025-11-11 16:10:39 +0100

    fixed scaling issue when collecting shells

--- scripts/shell.gd
+++ scripts/shell.gd
@@ -6,16 +6,16 @@ extends RigidBody2D
 
 func collect(agent: Agent) -> void:
 	ability.agent = agent
-	reparent(agent.shell_origin, true)
 	process_mode = Node.PROCESS_MODE_DISABLED
 	position = Vector2.ZERO
 	rotation = 0
+	reparent(agent.shell_origin, false)
+	is_collectable = false
 
 
 func drop(impulse: Vector2) -> void:
 	process_mode = Node.PROCESS_MODE_INHERIT
 	EventBus.drop_shell.emit(self)
 	apply_central_impulse(impulse)
-	is_collectable = false
 	await get_tree().create_timer(1).timeout
 	is_collectable = true
