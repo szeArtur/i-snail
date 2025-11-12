2839b226c713c99d21af17d45f5f0a4171db7003
szeArtur <artur.szeczinowski@proton.me>
2025-11-12 15:05:19 +0100

    fixed intro skipping

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,8 +1,14 @@
 extends VideoStreamPlayer
 
+
+var skipped: bool = false
+
+
 func switch_to_menu() -> void:
 	GameManager.change_state(GameManager.GameState.MAIN_MENU)
 
 
 func _unhandled_key_input(_event: InputEvent) -> void:
-	$AnimationPlayer.play("skip")
+	if not skipped:
+		skipped = true
+		$AnimationPlayer.play("skip")
