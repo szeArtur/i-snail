89f70f0aed9e18319fcdfb42f84f75f8cd858dce
szeArtur <artur.szeczinowski@proton.me>
2025-11-12 13:23:04 +0100

    made intro skipable

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -2,3 +2,7 @@ extends VideoStreamPlayer
 
 func switch_to_menu() -> void:
 	GameManager.change_state(GameManager.GameState.MAIN_MENU)
+
+
+func _unhandled_key_input(_event: InputEvent) -> void:
+	$AnimationPlayer.play("skip")
